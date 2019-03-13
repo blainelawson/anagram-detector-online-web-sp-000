feca56a90653349ea460e9d9af3a58d33dfd44d4
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-13 20:11:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   def match(word_array)
     # binding.pry
     word_array.collect do |array_word|
-      @word.split("").sort == array_word.split("").sort
+      @word.split("").sort == array_word.split("").sort ? array_word : nil
     end
   end
 
